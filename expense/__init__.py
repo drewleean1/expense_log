1a36ec1694da2f51b6edabc5cca0e9dbf9ba472d
drewleean1 <drewleean@gmail.com>
2024-04-04 13:22:38 -0600

    added port

--- expense/__init__.py
+++ expense/__init__.py
@@ -2,33 +2,34 @@ import os
 
 from flask import Flask
 
-def create_app(test_config = None): 
-    app = Flask(__name__, instance_relative_config = True)
-    app.config.from_mapping(
-        SECRET_KEY='dev', 
-        DATABASE=os.path.join(app.instance_path, 'expense.sqlite'),
-    )
+test_config = None 
+app = Flask(__name__, instance_relative_config = True)
+app.config.from_mapping(
+    SECRET_KEY='dev', 
+    DATABASE=os.path.join(app.instance_path, 'expense.sqlite'),
+)
 
-    if test_config is None: 
-        app.config.from_pyfile('config.py', silent=True)
-    else: 
-        app.config.from_mapping(test_config)
+if test_config is None: 
+    app.config.from_pyfile('config.py', silent=True)
+else: 
+    app.config.from_mapping(test_config)
 
-    try: 
-        os.makedirs(app.instance_path)
-    except OSError: 
-        pass
+try: 
+    os.makedirs(app.instance_path)
+except OSError: 
+    pass
 
-    from . import db
-    db.init_app(app)
+from . import db
+db.init_app(app)
 
-    from . import auth 
-    from . import record 
+from . import auth 
+from . import record 
 
-    app.register_blueprint(auth.bp)
-    app.register_blueprint(record.bp)
+app.register_blueprint(auth.bp)
+app.register_blueprint(record.bp)
 
-    app.add_url_rule('/personal', endpoint='personal')
+app.add_url_rule('/personal', endpoint='personal')
 
-    return app 
 
+if __name__ == "__main__": 
+    app.run(host='0.0.0.0', port = int(os.environ.get('PORT', 33507)))
